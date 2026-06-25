2526c4672e8c643403966cec204f3ac3760ea332
Yuvraj Dave <dev.yuvraj.in@gmail.com>
2026-06-25 16:01:22 +0530

    PosgreSQL

---
+++ Connection.py
@@ -0,0 +1,23 @@
+import psycopg2 
+
+conn = psycopg2.connect(
+    host="localhost",
+    dbname="students",
+    user="postgres",
+    password="1234",
+    port=5432
+)
+
+cur = conn.cursor()
+
+cur.execute("SELECT * FROM student_info;")
+
+rows = cur.fetchall()  # Get all rows
+
+for row in rows:
+    print(row)
+
+conn.commit()
+
+cur.close()
+conn.close()
